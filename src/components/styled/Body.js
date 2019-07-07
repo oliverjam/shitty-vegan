c4f46ce011d5727f345654ff68b71d7bbc7d6ef5
oliverjam <oliverjamesphillips@gmail.com>
2019-07-07 23:39:32 +0100

    Remove unused files

--- src/components/styled/Body.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled/macro';
-
-export const Body = styled.body`
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
-    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  font-size: 1.125em;
-  font-size: calc(1em + 0.25vw);
-  color: ${{theme} => theme.textDark};
-`;
